ceabde5983eae88411f48ed7a2d6327d0f0489a1
theHawk (Doug Hawkinson) <doug5solas@gmail.com>
2017-06-10 15:16:36 -0700

    Seeking help - problem with res.json

--- src/app.js
+++ src/app.js
@@ -1,28 +1,47 @@
+//  TTH.com - Express Basics Course
 'use strict';
 
-var express = require('express'),
+let express = require('express'),
     posts   = require('./mock/posts.json');
 
-var app = express();
+//  JS pattern: turns obj => array
+let postsLists = Object.keys(posts).map(function(value) {
+    return posts[value];
+});
 
-app.set('view engine', 'jade');
-app.set('views', __dirname + '/templates')
+let app = express();
 
-app.get('/', function(req, res){
+app.use('/static',express.static(__dirname + '/public'));
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/templates');
+
+app.get('/', function(req, res) {
+    let path = req.path;
+    res.locals.path = path;
     res.render('index');
 });
 
-app.get('/blog/:title?', function(req, res){
-    var title = req.params.title;
+app.get('/blog/:title?', function(req, res) {
+    let title = req.params.title;
     if (title === undefined) {
         res.status(503);
-        res.send("This page is under construction!");
+        res.render('blog', {posts:postsLists});
     } else {
-        var post = posts[title] || {};
+        let post = posts[title] || {};
         res.render('post', { post: post});
     }
 });
 
+app.get('/posts', function(req, res) {
+    if (req.query.raw) {
+        res.json(posts);
+    } else {
+        res.json(postsLists);
+    }
+});
+
 app.listen(3000, function() {
+    'use strict';
     console.log("The frontend server is running on port 3000!");
 });
